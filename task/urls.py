32c6f245aac70f5d7f6f1e06495ac40dd1013e9d
letocia6672 <leticiasimao143@gmail.com>
2026-04-06 20:48:22 -0300

    adicionamos a opção nova tarefa

--- task/urls.py
+++ task/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, task_list
+from .views import index, task_list, create
 
 urlpatterns = [
     path('', index, name='index'),
     path('tasks/', task_list, name='tasks'),
+    path('create', create, name="create"),
 ]
--- task/views.py
+++ task/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from task.models import Tarefa
 
 def index(request):
     return render(request, 'task/index.html')
 
 def task_list(request):
-    return render(request, 'task/tasks.html')
+    lista = Tarefa.objects.all()
+    return render(request, 'task/tasks.html', {'tarefas': lista})
+
+def create(request):
+    if request.method == 'POST':
+        titulo = request.POST.get('titulo')
+        descricao = request.POST.get('descricao')
+        data =request.POST.get('data')
+        status = False
+        Tarefa.objects.create(tarefa=titulo, descricao=descricao, data=data, status=status)
+        return redirect('tasks')
+    return render(request, 'task/create.html')
